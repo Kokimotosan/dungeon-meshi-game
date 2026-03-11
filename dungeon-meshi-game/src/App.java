c6786363f5531fdfde1ee3e8605c74b9a10f7716
Angelo <julio.dasilva.telles@gmail.com>
2026-03-10 22:29:51 -0300

    alteração na logica de impressão de logs entre turnos, alteração de logica geral do coodigo, implementação de swicht cases para tanto turnos do heroi quanto para do inimigo e organização do codigo

--- dungeon-meshi-game/src/App.java
+++ dungeon-meshi-game/src/App.java
@@ -28,24 +28,24 @@ public class App {
     
     public static void main(String[] args) throws Exception {
         // Objetos iniciais
-        Hero Laios = new Hero("Laios", 20, 0, 2);
-        Enemy mushroom1 = new Enemy("Cogumelo Andante", 8, 0);
-        Enemy mushroom2 = new Enemy("Cogumelo Andante", 8, 0);
-        DamageCard swordCard = new DamageCard("Espada", 3, 1);
+        Hero Laios = new Hero("Laios", 20, 0, 1);
+        Enemy mushroom1 = new Enemy("Cogumelo Andarilho", 8, 0);
+        Enemy mushroom2 = new Enemy("Cogumelo Andarilho", 8, 0);
+        DamageCard swordCard = new DamageCard("Espada", 4, 1);
         ShieldCard shieldCard = new ShieldCard("Escudo Pequeno", 3, 1);
 
         // Variaveis
         int round = 1;
         int turn = 0;
         int energy_max = 1 + Laios.energy_mod;
-        int energy = energy_max;
         
         // Loop do combate
         while (Laios.isAlive() || (mushroom1.isAlive() && mushroom2.isAlive())){
             // Printando a telinha da batalha
             displayBattleState(round, Laios, mushroom1, mushroom2);
+            
             boolean endTurn = false;
-
+            int energy = energy_max;
             turn = turn % 3;
 
             if (turn == 0){ // Turno do Herói
@@ -58,35 +58,62 @@ public class App {
                 // Loop de escolha de ação
                 do {
                     System.out.println("Escolha uma ação: ");
-                    System.out.println("(1) Usar " + swordCard.name);
-                    System.out.println("(2) Usar " + shieldCard.name);
+                    System.err.println("Energia: " + energy + "/" + energy_max);
+                    System.out.println("(1) Usar " + swordCard.name + " | Custo: " + swordCard.cost);
+                    System.out.println("(2) Usar " + shieldCard.name + " | Custo: " + shieldCard.cost);
                     System.out.println("(3) Encerrar turno");
                     boolean target = false;
                     switch(receiveInput()){
                         case 1:
-                            System.out.println("===== Escolha um alvo =====");
-                            System.out.println("(1) Cogumelo Andante" + mushroom1.health + "/" + mushroom1.max_health);
-                            System.out.println("(2) Cogumelo Andante" + mushroom2.health + "/" + mushroom2.max_health);
-                            System.out.println("(3) Retornar ao menu de ação");
                             do {
+                                System.out.println("===== Escolha um alvo =====");
+                                System.out.println("(1) Cogumelo Andarilho" + mushroom1.health + "/" + mushroom1.max_health);
+                                System.out.println("(2) Cogumelo Andarilho" + mushroom2.health + "/" + mushroom2.max_health);
+                                System.out.println("(3) Retornar ao menu de ação");
                                 switch (receiveInput()){
                                     case 1:
                                         mushroom1.takeDamage(swordCard.damage);
-                                        System.out.println("Cogumelo Andante recebeu " + swordCard.damage);
+                                        System.out.println("Cogumelo Andarilho recebeu " + swordCard.damage + "de dano");
+                                        energy -= swordCard.cost;
                                         target = true;
                                         break;
                                     case 2:
                                         mushroom2.takeDamage(swordCard.damage);
-                                        System.out.println("Cogumelo Andante recebeu " + swordCard.damage);
+                                        System.out.println("Cogumelo Andarilho recebeu " + swordCard.damage + "de dano");
+                                        energy -= swordCard.cost;
+                                        target = true;
+                                        break;
+                                    case 3:
                                         target = true;
                                         break;
                                     default:
+                                        clearScreen();
                                         System.out.println("Opção invalida!");
                                         break;
                                 }
                             } while (target);
                             break;
                         case 2:
+                            do {
+                                System.out.println("===== Escolha um alvo =====");
+                                System.out.println("(1) Conceder " + shieldCard.shield + " para Laios");
+                                System.out.println("(2) Retornar ao menu de ação");
+                                switch (receiveInput()){
+                                    case 1:
+                                        Laios.gainShield(shieldCard.shield);
+                                        System.out.println("Laios recebeu" + shieldCard.shield + "de escudo");
+                                        energy -= shieldCard.cost;
+                                        target = true;
+                                        break;
+                                    case 2:
+                                        target = true;
+                                        break;
+                                    default:
+                                        clearScreen();
+                                        System.out.println("Opção invalida!");
+                                        break;
+                                }
+                            } while (target);
                             break;
                         case 3:
                             endTurn = true;
@@ -95,8 +122,14 @@ public class App {
                             System.out.println("Opção invalida!");
                             break;
                     }
+                    clearScreen();
                 } while (energy == 0 || endTurn);
-            }
+            } else {
+                mushroom1.attack(Laios, 3);
+                System.out.println("Laios recebeu uma cabeçada de um Cogumelo Andarilho");
+                clearScreen();
+            }   
         }
     }
-}
+ }
+
