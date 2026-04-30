a93ebfa7de59110c375f8c716fb8e00fb02c3ae6
Angelo <julio.dasilva.telles@gmail.com>
2026-04-30 20:55:29 -0300

    classes inventario e Items

--- app/src/main/java/dungeonmeshigame/Inventory.java
+++ app/src/main/java/dungeonmeshigame/Inventory.java
@@ -1,14 +1,14 @@
 package dungeonmeshigame;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 public class Inventory {
     int mxCapacity;
     int capacity;
-    Map<Item, Integer> items;
+    HashMap<Item, Integer> items;
 
-    public Inventory(int capacity, int mxCapacity, Map<Item, Integer> items){
+    public Inventory(int capacity, int mxCapacity, HashMap<Item, Integer> items){
         this.mxCapacity = mxCapacity;
         this.capacity = capacity;
         this.items = items;
@@ -25,17 +25,51 @@ public class Inventory {
     public void displayInventory(){
         System.out.println("-------- Seu Inventario --------");
         System.out.println("-------- Capacidade " + this.mxCapacity + "/" + this.capacity + " --------");
-        for (Item item : this.items){
-            item.displaytItem();
+        this.items.forEach((item, quantity) -> {
+            System.out.println(item.name + " (x" + quantity + ") Pesa: " + item.weight);
+        });
+    }
+
+    public Item searchItem(String name){
+        Item searched = null;
+        boolean found = false;
+        while (!found) {
+            for (Item item : this.items.keySet()){
+                if (item.getName().equals(name)){
+                    searched = item;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                System.out.println("Esten Item não existe no inventario, escolha outro");
         }
+        return searched;
     }
 
     public void removeItem(){
-        
+        Scanner scan = new Scanner(System.in);
+        displayInventory();
+        System.out.println("Digite o Nome do Item que deseja remover");
+        String name = scan.nextLine();
+        Item removed = searchItem(name);
+        if (this.items.getOrDefault(removed, 0) > 0)
+            this.items.computeIfPresent(removed, (key, quantity) -> quantity - 1);
+        else
+            this.items.remove(removed);    
     }
 
-    public void editInventory(){
-
+    public void EditInventory(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("(0) Remova um item");
+        System.out.println("(1) Retorne");
+        int choice = scan.nextInt();
+        if (choice == 0)
+            this.removeItem();
+        else if (choice == 1)
+            return;
+        else 
+            System.out.println("Escolha outra opção");           
     }
 
     void addItem(Item nItem){
